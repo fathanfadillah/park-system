4ba30a36a539d1dd1545a8a29fddfa0306778d01
fathanfadillah <fathan17698@gmail.com>
2022-08-29 21:49:01 +0700

    add page for admin and user

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+ 
+@section('content')
+<div class="container">
+    <p class="navbar-brand" href="#">Welcome, Admin</p>
+    <a class="btn btn-block btn-primary" href="">Report</a>
+</div>
+@endsection
