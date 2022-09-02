7a59f76d15040bf86fdcf58b5324022a09f34e8d
fathanfadillah <fathan17698@gmail.com>
2022-09-02 19:12:47 +0700

    add code for dropping roles table

--- database/migrations/2022_08_29_115712_update_column_id_to_user_id_in_roles_table.php
+++ database/migrations/2022_08_29_115712_update_column_id_to_user_id_in_roles_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('roles');
         Schema::create('roles', function (Blueprint $table) {
             $table->id('user_id');
             $table->string('name');
