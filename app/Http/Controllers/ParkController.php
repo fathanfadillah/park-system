5f63277f7dd676f0ecd5e24fdb673c75cc80bab3
fathanfadillah <fathan17698@gmail.com>
2022-09-02 17:01:29 +0700

    add validation when input park and save park data

--- app/Http/Controllers/ParkController.php
+++ app/Http/Controllers/ParkController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Faker\Generator as Faker;
+
+use App\Models\Park;
 
 class ParkController extends Controller
 {
@@ -11,34 +15,56 @@ class ParkController extends Controller
     }
 
     public function validateEnterPark(Request $request) {
-        $validate = $request->validate([
-            'policyNumber' => ['required'],
-        ]);
-
-        if (!$validate) {
-            return view('user.home')->with('error', 'policy number is required');
+        $error = null;
+        $required = $request->validate([
+                'policyNumber' => ['required'],
+            ]);
+        if (!$required) {
+            $error = 'policy number is requried';
         }
 
-        $policyNumber = $request->policyNumber;
-        
-        $validatedPolicyNumber = self::validatePolicyNumber($policyNumber);
+        $policyNumber = $request->policyNumber;    
+        $validatedPolicyNumber = self::validatePolicyNumber($policyNumber);    
         if (!$validatedPolicyNumber) {
-            return view('user.home')->with('error', 'invalid policy number');
+            $error = 'policy number is invalid';
         }
-        
+
+        if ($error) {
+            return $error;
+        }
     }
 
     public function validatePolicyNumber($policyNumber) {
-        $pattern = '/^[A-Z]{1,2}-[0-9]{1,4}-[A-Z]{1,3}$/';
+        $pattern = '/[A-Z]{1,2}-[0-9]{1,4}-[A-Z]{1,3}/';
         $match = preg_match($pattern, $policyNumber);
         return $match;
     }
 
-    public function enterPark(Request $request) {
-        self::validateEnterPark($request);
+    public function enterPark(Request $request, Faker $faker) {
+        $validate = self::validateEnterPark($request);
+        if ($validate) {
+           return back()->with('error', $validate);
+        }
+        
+        $userId = Auth::user()->id;
         $policyNumber = $request->policyNumber;
-        $timestampNow = date('Y-m-d H:i:s');
+        $timeNow = date('Y-m-d H:i:s');
+
+        $parkCode = $faker->uuid();
+
+        $park = Park::create([
+            'park_code' => $parkCode,
+            'user_id' => $userId,
+            'policy_number' => $policyNumber,
+            'enter_time'=> $timeNow,
+        ]);
+
+        $parkHistory = $park->histories()->create([
+            'park_id' => $park->id,
+            'time' => $timeNow,
+            'status' => 'ENTER'
+        ]); 
+
+        return redirect()->route('user.home');
     }
-
-
 }
